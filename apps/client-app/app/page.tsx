ec7b91796f84a605b0d85bfaf85eda1466836a50
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-10-21 12:28:17 +0700

    Finished NavbarComponent

--- apps/client-app/app/page.tsx
+++ apps/client-app/app/page.tsx
@@ -1,12 +1,9 @@
-import { Input } from "ui-components";
+import NavbarComponent from "./components/NavbarComponent";
 export default function Home() {
   return (
     <>
-      <div className="m-4 p-4">
-        <h1 className="font-bold text-xl underline text-red-700 mb-4">
-          Client-App
-        </h1>
-        <Input />
+      <div className="">
+        <NavbarComponent />
       </div>
     </>
   );
