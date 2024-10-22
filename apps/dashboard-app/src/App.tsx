184823a5953e3bf62b9795f62976032a537fc1ab
Mab Sothea <sotheamab002@gmail.com>
2024-10-22 09:15:23 +0700

    Added: Fixed storybook

---
+++ apps/dashboard-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "ms-ui-components";
+function App() {
+  return (
+    <>
+    <h1 className="flex justify-center text-3xl">Just in, dashboard-app</h1>
+    <Button/>
+    </>
+  );
+}
+
+export default App;
