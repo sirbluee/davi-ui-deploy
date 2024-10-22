5e91fd4c634633a5cebdec4e964a25bfd5b42a2c
Smey <roemreaksmey7@gmail.com>
2024-10-22 14:11:39 +0700

    fix respone on mobile

--- apps/client-app/app/page.tsx
+++ apps/client-app/app/page.tsx
@@ -1,15 +1,13 @@
-// import { Input } from "ui-components";
+import { Input } from "ui-components";
 import AboutPage from "./about/page";
 export default function Home() {
   return (
     <>
-      {/* <div className="m-4 p-4"></div> */}
-      <div>
-        {/* <h1 className="font-bold text-xl underline text-red-700 mb-4">
+      <div className="m-4 p-4">
+        <h1 className="font-bold text-xl underline text-red-700 mb-4">
           Client-App
         </h1>
-        <Input /> */}
-        <AboutPage />
+        <Input />
       </div>
     </>
   );
