af11b98e2d45af0530f01d95846791c33593a4ec
Smey <roemreaksmey7@gmail.com>
2024-10-22 15:28:55 +0700

    add hero section

--- apps/client-app/app/about/page.tsx
+++ apps/client-app/app/about/page.tsx
@@ -8,13 +8,21 @@ import about_image_3 from "@/public/images/about/about_image_3.png";
 import { mockData, features_1, features_2, teamMembers } from "./mockData";
 import BrainStormCard from "@/app/about/components/brainStomCard";
 import FeatureCard from "@/app/about/components/FeaturesListCard";
-import bgTeam from "@/public/images/about/background/bgteam.png";
-import { TeamMemberCard } from "@/app/about/components/TeamMemberCard";
 import { Button } from "@/app/about/components/atoms/Button";
+import NavbarComponent from "../components/NavbarComponent";
+import { HeroSection } from "./components/blockCode/HeroSection";
+import { OurTeam } from "./components/blockCode/OurTeam";
 
 export default function AboutPage() {
   return (
     <div>
+      {/* Navbar */}
+      <div className="absolute w-full z-50">
+        <NavbarComponent />
+      </div>
+
+      {/* Hero Section */}
+      <HeroSection />
       {/* Block 1 */}
       <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-10 p-4 lg:p-12 items-center justify-center">
         <div className="flex-shrink-0 transform transition-transform duration-300 hover:scale-105">
@@ -31,7 +39,7 @@ export default function AboutPage() {
             Solutions for Your Large Enterprise
           </h2>
           <p className="mb-6 text-lg font-light text-gray-600 text-center lg:text-left">
-            Empower your business with DAVI’s cutting-edge analytics platform,
+            Empower your business with DAVI's cutting-edge analytics platform,
             offering fast data processing and secure cloud solutions, tailored
             for scalability and performance.
           </p>
@@ -131,45 +139,8 @@ export default function AboutPage() {
         ))}
       </div>
 
-      {/* Block 4 */}
-      <div className="relative w-full py-10 lg:py-20">
-        {/* Background Image */}
-        <Image
-          src={bgTeam}
-          alt="bgTeam"
-          layout="fill"
-          objectFit="cover"
-          className="z-0"
-        />
-        <div className="relative z-10 flex flex-col items-center text-center px-4 sm:px-10">
-          <div className="pb-6 sm:pb-10 space-y-4 sm:space-y-6">
-            <h1 className="text-3xl sm:text-4xl font-bold text-white">
-              Our Team
-            </h1>
-            <p className="text-sm sm:text-lg text-gray-200">
-              Our dedicated team of experts is committed to delivering
-              innovative data solutions that drive business success.
-            </p>
-          </div>
-
-          {/* Team Members Grid */}
-          <div className="flex flex-col items-center space-y-6 px-4 sm:px-0">
-            {/* First Row with 4 items */}
-            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 sm:gap-6">
-              {teamMembers.slice(0, 4).map((member) => (
-                <TeamMemberCard key={member.id} member={member} />
-              ))}
-            </div>
-
-            {/* Second Row with 3 items */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6">
-              {teamMembers.slice(4, 7).map((member) => (
-                <TeamMemberCard key={member.id} member={member} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* Block 4 our Team */}
+      <OurTeam />
     </div>
   );
 }
