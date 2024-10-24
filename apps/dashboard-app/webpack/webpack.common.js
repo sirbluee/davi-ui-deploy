404378a0501e7ce8897f123598428fbb3e5c2c0a
Mab Sothea <sotheamab002@gmail.com>
2024-10-24 16:23:38 +0700

    Added: Issue, cannot to link node_modules

--- apps/dashboard-app/webpack/webpack.common.js
+++ apps/dashboard-app/webpack/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, "..", "./public/index.html"),
     }),
   ],
 };
