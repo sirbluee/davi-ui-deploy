cb27b0e675fd07d2ccf45e654cc3e85eff161948
oengzhileang <zhileangCC990@gmail.com>
2024-10-25 14:22:59 +0700

    send email[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[Cdo se[2~en[C[C[C[C[C[C[B[A[D[D[Ddsda[C[C[C[C[C[C[C[C[C[C[C[C[C[Dasd[D[D[Ddo send email

--- packages/ui-components/index.ts
+++
@@ -1,5 +0,0 @@
-import Input from './src/components/atoms/Input';
-import Button from './src/components/atoms/Button';
-
-
-export { Input, Button }
--- packages/ui-components/src/components/atoms/Button.tsx
+++
@@ -1,3 +0,0 @@
-const Button = () => <button className='text-3xl font-bold underline text-red-400 bg-slate-300 p-2'>Sad, Storybook</button>;
-
-export default Button;
