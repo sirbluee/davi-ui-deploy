1b468c2916d9421a5d1491604b13a82e4e8d1bf9
sirblue <sophearum14@gmail.com>
2024-10-28 14:07:42 +0700

    modified db structure

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -1,10 +1,9 @@
-import { ClickCounter } from "./ClickCounter";
 import "./styles.css";
 export const App = () => {
   return (
-    <div>
-      <h1>
-        <ClickCounter />
+    <div className="-screen flex flex-col justify-center items-center">
+      <h1 className="text-red-500">
+        Home page
       </h1>
     </div>
   );
