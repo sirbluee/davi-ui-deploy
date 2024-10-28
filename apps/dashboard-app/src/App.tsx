19e7383d0a561a567f9fffe7e681c5b9e596fa3c
Mab Sothea <sotheamab002@gmail.com>
2024-10-28 09:14:14 +0700

    Added: ..........

---
+++ apps/dashboard-app/src/App.tsx
@@ -0,0 +1,13 @@
+// import { Button } from "ms-ui-components";
+import { ClickCounter } from "./ClickCounter";
+import "./styles.css";
+export const App = () => {
+  return (
+    <div>
+      <h1>
+        <ClickCounter />
+        {/* <Button label="Click O mk"/> */}
+      </h1>
+    </div>
+  );
+};
