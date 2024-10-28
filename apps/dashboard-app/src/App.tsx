ec109347c773405558e2efd18035674453d093b9
sirblue <sophearum14@gmail.com>
2024-10-28 16:17:59 +0700

    modified layout

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -1,9 +1,9 @@
-import Layout from "./components/layout/Layout";
+import Layout from "./layout/MainLayout";
 import "./styles.css";
 export const App = () => {
   return (
     <Layout>
-      <div>
+      <div className="ml-[110px]">
         {/* Content goes here */}
         <h1 className="text-2xl font-bold">Welcome to my Team</h1>
       </div>
--- apps/dashboard-app/src/layout/MainLayout.tsx
+++ apps/dashboard-app/src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
