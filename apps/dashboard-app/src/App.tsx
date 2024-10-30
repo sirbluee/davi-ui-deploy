f9d79336160c297a92e9931941ee3cdc255a6e39
oengzhileang <zhileangCC990@gmail.com>
2024-10-30 13:30:18 +0700

    dynamic button

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -1,12 +1,29 @@
 import Layout from "./layout/MainLayout";
 import "./styles.css";
 import DisplayTable from "./components/DisplayTable";
+import ShowTable from "./components/ShowTable";
+import Button from "./components/atoms/Button";
+
+
 export const App = () => {
   return (
     <Layout>
       <div className="ml-[110px]">
         {/* Content goes here */}
-        <h1 className="text-2xl font-bold">Welcome to my Team</h1>
+        {/* <h1 className="text-2xl font-bold">Welcome to my Team</h1> */}
+        <div className="flex mx-5 mt-5">
+          <DisplayTable />
+          <ShowTable />
+        </div>
+        <div className="flex justify-end mx-5 mt-5">
+          <Button
+            children="Next"
+            radius="large"
+            size="medium"
+            variant="solid"
+            color="primary"
+          />
+        </div>
       </div>
     </Layout>
   );
