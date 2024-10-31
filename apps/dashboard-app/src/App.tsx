276dd9dbbfbc982dda7623af59da576d8fb645f2
sirblue <sophearum14@gmail.com>
2024-10-31 14:15:39 +0700

    modified structure page

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -3,18 +3,29 @@ import Layout from "@/src/components/organisms/layout/MainLayout";
 import "@/src/styles.css";
 import Button from "./components/atoms/Button";
 import { InputTest } from "./components/atoms/Input";
-import CleaningPage from "./components/pages/CleaningPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Visualize from "./components/pages/Visualize";
+import Dataset from "./components/pages/Dataset";
+import Project from "./components/pages/Project";
+import Helps from "./components/pages/Helps";
 
 export const App = () => {
   return (
-    <Layout>
-      <div className="ml-[110px]">
-        <CleaningPage/>
-        {/* <TableTest/>
-        <Button children={"Next"} size="medium" radius="2xl" isLoading={false} color="outline" isIconOnly={false}/> */}
-      </div>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Project />} />
+          <Route path="/visualize" element={<Visualize />} />
+          <Route path="/dataset" element={<Dataset />} />
+          <Route path="/helps" element={<Helps />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 };
-
 export default App;
---
+++ apps/dashboard-app/src/components/pages/Dataset.tsx
@@ -0,0 +1,7 @@
+export default function Dataset() {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <h1>Dataset Page</h1>
+      </div>
+    );
+  }
