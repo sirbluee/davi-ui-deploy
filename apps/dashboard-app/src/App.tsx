ed22dab0dad2c79a2e64a671bbe19c01d2fe451c
sirblue <sophearum14@gmail.com>
2024-11-01 16:16:02 +0700

    fixed layout

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -1,6 +1,13 @@
 import Layout from "@/src/components/organisms/layout/MainLayout";
 import "./styles.css";
-import { BrowserRouter as Router, Route, BrowserRouter, Routes } from "react-router-dom";
+import Button from "./components/atoms/Button";
+import { InputTest } from "./components/atoms/Input";
+import {
+  BrowserRouter as Router,
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 import Visualize from "./components/pages/Visualize";
 import Dataset from "./components/pages/Dataset";
 import Project from "./components/pages/Project";
@@ -10,10 +17,9 @@ import AccountSettings from "./components/templates/AccountSetting";
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Project />} />
-          <Route path="/resule-cleanig" element={<ShowResuleCleaning />} />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Project />} />
           <Route path="/visualize" element={<Visualize />} />
           <Route path="/dataset" element={<Dataset />} />
           <Route path="/helps" element={<Helps />} />
--- apps/dashboard-app/src/components/organisms/layout/MainLayout.tsx
+++ apps/dashboard-app/src/components/organisms/layout/MainLayout.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "@/src/components/organisms/Header";
 import Sidebar from "@/src/components/organisms/Sidebar";
-import { Outlet } from "react-router-dom";
 
-const Layout: React.FC = () => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col ml-[7%]">
+      <div className="flex-1">
         <Header />
         <main className="p-6 mt-10 ml-28">{children}</main>
       </div>
